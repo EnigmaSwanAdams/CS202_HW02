a6fc18e9c84e9dbc5cdbddef464584a24b39c084
Enigma Swan Adams <eswanadams@alaska.edu>
2022-02-20 18:06:24 -0900

    created myValue class

--- myValue.cpp
+++ myValue.cpp
@@ -13,12 +13,40 @@
 
 #include "myValue.h"
 
-// constructors
-// defualt contructor
-// quick constructor 
-// copy constructor
-/// destructor
+
+// Copy constructor
+myValue::myValue(const myValue& orig) : _daysSW(orig._daysSW), //  copy
+_potSize(orig._potSize), _name(orig._name), _potDescript(orig._potDescript), _plantDescript(orig._plantDescript),
+_location(orig._location), _dateA(orig._dateA){}
+
+// Quick constructor 
+myValue::myValue(int daysSW, float potSize, string name, string potDescript,
+	string plantDescript, string location, vector<int> dateA) : _daysSW(daysSW), _potSize(potSize),
+	_name(name), _potDescript(potDescript), _plantDescript(plantDescript), _location(location),
+	_dateA(dateA) {}
+// Quick constructor partial ( feel like ther is a better way to handle this by taking a variable number of
+// parameters somehow, but I can't recal 
+myValue::myValue(int daysSW, float potSize, string name) : _daysSW(daysSW), _potSize(potSize),
+_name(name) {}
 
 
-// member functions
+//member functions
+//printing plant infor functions
+void myValue::printValueBasic(){  // pot size, dats since watered, and name 
+	cout << _name << " is in a " << _potSize << " inch pot and its been " << _daysSW <<
+		" days since you watered them" << endl;}
+void myValue::printValueFindInnfo() {// what it looks like and where it is
+	cout << "location desciption: " << _location << endl;
+	cout << "Plant description: " << _plantDescript << endl;
+	cout << "Pot description: " << _potDescript << endl;}
+void myValue::printDate() { // prints the date the plant was aquired
+	cout << "This plant was aquired in " << _dateA.at(0) << "/" << _dateA.at(1) << "/" << _dateA.at(3) << endl;
+}
+
+//unfinished 
+//void changeVar();
+
+// operator overloads
+
+
 
--- myValue.h
+++ myValue.h
@@ -7,29 +7,60 @@
 *file containing header for Value class 
 *has class declarations for member functions
 *and variables
+* 
+*  I'm going to reuse some of my plant class from a previous lab
+* and add some more fields (members) to
 */
 
 
 #include <iostream>
+#include <string>
+#include <vector>
+using std::vector;
+using std::string;
 using std::cout;
 using std::endl;
 
 #ifndef myValue_H_
 #define myValue_H_
 // class declaration goes here
-class Value
+class myValue
 {
 public:
-	// defualt contructor
-	// copy constructor 
-	// quick constructor 
-	// destructor
+
+	// Copy constructor
+	myValue(const myValue& orig);
+
+	// Quick constructor (intitialized you values)
+	myValue(int daysSW, float potSize, string name, string potDescript, 
+		string plantDescript, string location, vector<int> dateA);
+	// partial version 
+	// Quick constructor partial
+	myValue(int daysSW, float potSize, string name);
+
+
+	//member functions
+	// prints all plant info
+	void printValueBasic();
+	void printValueFindInnfo(); // what it looks like and where it is
+	void printDate(); // prints the date the plant was aquired
+
+	//unfinished 
+	//void changeVar();
 
 	// operator overloads
-	
-
 
 private:
+	int _daysSW = -1;
+	float _potSize = -1;
+	string _name = "NOT NAMED"; // genus a species would be more usefull but I don't know those things off the top
+	// of my head so I'll leave it out 
+	string _potDescript = "UNDEFINED"; // maybe it would be one of a fiew options managed by a user defined type later 
+	// but this is fine for now
+	string _plantDescript = "UNDEFINED";
+	string _location = "UNDEFINED"; // later it would be cool to have a more automated system for location 
+	vector<int> _dateA = { 0, 0, 0 }; // array holding the date the plant was aquired 
+	// where the first value is the month, second is the day, and last is the year
 	
 };
 
